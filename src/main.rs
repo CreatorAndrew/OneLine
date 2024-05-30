eeab0420d9594aa6e51148f201b58a59b7bc6cbb
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2024-05-30 17:28:51 -0500

    fix paths

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::fs;
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 use std::process::Command;
@@ -45,12 +44,12 @@ fn drive(path: String) -> String {
     return drive;
 }
 
-fn exists(line: String, seg: String, os_drive: &str, root_dir: &str) -> bool {
+fn exists(line: String, segment: String, os_drive: &str, root_dir: &str) -> bool {
     if line.contains(":/") {
         return Path::new(
             &(
-                line[line.rfind(":/").unwrap() - 1..].replace("\"", "").replacen(os_drive, root_dir, 1).to_string() +
-                &seg.replacen(&drive(seg.to_string()), "", 1)
+                line[line.rfind(":/").unwrap() - 1..].replace("\"", "").replacen(os_drive, root_dir, 1).to_string()
+                + &segment.replacen(&drive(segment.to_string()), "", 1)
             )
         ).exists();
     }
@@ -58,16 +57,16 @@ fn exists(line: String, seg: String, os_drive: &str, root_dir: &str) -> bool {
 }
 
 fn command_found(program: &str) -> bool {
-    if let Ok(path) = env::var("PATH") {
+    if let Ok(path_env) = env::var("PATH") {
         let delimiter;
         if env::consts::OS == "windows" {
             delimiter = ";";
         } else {
             delimiter = ":";
         }
-        for p in path.split(delimiter) {
-            let p_str = format!("{}/{}", p, program);
-            if fs::metadata(p_str).is_ok() {
+        for path_dir in path_env.split(delimiter) {
+            let path = format!("{}/{}", path_dir, program);
+            if Path::new(&path).exists() {
                 return true;
             }
         }
@@ -78,6 +77,24 @@ fn command_found(program: &str) -> bool {
 fn working_dir() -> io::Result<PathBuf> {
     let working_dir = env::current_dir()?;
     Ok(working_dir)
+}
+
+fn apply_working_dir(path: &str) -> String {
+    let mut temp_path = path.replace("\\", "/");
+    let mut exe_path = working_dir().expect("").display().to_string().replace("\\", "/");
+    if env::consts::OS != "windows" {
+        exe_path = "Z:".to_string() + &exe_path;
+    }
+    if temp_path == ".".to_string() {
+        temp_path = exe_path;
+    } else if temp_path == "..".to_string() {
+        temp_path = exe_path[..exe_path.rfind("/").unwrap()].to_string();
+    } else if temp_path.starts_with("./") {
+        temp_path = temp_path.replacen("./", &(exe_path.to_string() + "/").as_str(), 1)
+    } else if temp_path.starts_with("../") {
+        temp_path = temp_path.replacen("../", &(exe_path[..exe_path.rfind("/").unwrap()].to_string() + "/").as_str(), 1)
+    }
+    return temp_path;
 }
 
 fn main() {
@@ -91,10 +108,6 @@ fn main() {
     } else {
         os_drive = "Z:/";
         root_dir = "/";
-    }
-    let mut here = working_dir().expect("").display().to_string().replace("\\", "/");
-    if env::consts::OS != "windows" {
-        here = "Z:".to_string() + &here;
     }
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
@@ -128,53 +141,19 @@ fn main() {
         index += 1;
     }
     for i in 0..args.len() {
-        let dot_dot = "PREVIOUS DIRECTORY PLACEHOLDER HERE";
         let mut line = "".to_string() + &args[i].replace("\\", "/");
-        let mut sublines: Vec<String> = Vec::new();
-        let temp:Vec<&str> = line.split(" ").collect();
-        for temp_line in temp {
-            if temp_line.contains("../") {
-                sublines.push(temp_line.to_string());
-            }
+        let temp_line = line.clone();
+        let sublines:Vec<&str> = temp_line.split(" ").collect();
+        line = "".to_string();
+        for subline in sublines {
+            line += &(apply_working_dir(subline) + " ").as_str();
         }
-        let mut path: Vec<String> = Vec::new();
-        let mut previous: Vec<String> = Vec::new();
-        for j in 0..sublines.len() {
-            path.push("".to_string() + &here);
-            previous.push("".to_string());
-            let temp = "".to_string() + &sublines[j];
-            while sublines[j].contains("../") {
-                sublines[j] = sublines[j].replacen("../", "", 1);
-                path[j] = path[j][..path[j].rfind("/").unwrap()].to_string();
-                previous[j] += "../";
-            }
-            if previous[j].is_empty() {
-                previous[j] = "../".to_string();
-            }
-            path[j] += "/";
-            if temp.contains("/../") {
-                if !temp.contains(&previous[j]) {
-                    panic!("Support for nonconsecutive instances of `../` in the path has not yet been implemented.")
-                }
-                if temp.find("/../").unwrap() > 1 {
-                    if temp[temp.find("/../").unwrap() - 2..temp.find("/../").unwrap()].to_string() == ".." {
-                        line = line.replacen(&previous[j], &path[j], 1);
-                    } else {
-                        line = line.replacen("../", dot_dot, 1);
-                    }
-                } else {
-                    line = line.replacen("../", dot_dot, 1);
-                }
-            } else {
-                line = line.replacen(&previous[j], &path[j], 1);
-            }
-        }
-        line = line.replace("./", &(here.to_string() + "/")).replace(dot_dot, "../") + "/";
         let mut segments: Vec<String> = Vec::new();
         while line.contains("/") {
             segments.push(line[..line.find("/").unwrap()].to_string());
             line = line[line.find("/").unwrap() + 1..].to_string();
             if !line.contains("/") {
+                segments.push(line[..line.len() - 1].to_string());
                 line = "".to_string();
             }
         }
@@ -184,10 +163,10 @@ fn main() {
                 quote = exists(line.clone(), "".to_string() + segments[j].as_str(), os_drive, root_dir) || command_found(segments[j].as_str());
             }
             if quote {
-                let temp = "".to_string() + &segments[j];
+                let temp_segment = "".to_string() + &segments[j];
                 segments[j] = "\"".to_string() + &segments[j].replace(&drive("".to_string() + &segments[j]), "") + "\"";
-                if temp.contains(&drive("".to_string() + &temp)) {
-                    segments[j] += &drive("".to_string() + &temp);
+                if temp_segment.contains(&drive("".to_string() + &temp_segment)) {
+                    segments[j] += &drive("".to_string() + &temp_segment);
                 }
             }
             line += &segments[j];
@@ -201,7 +180,7 @@ fn main() {
             if line.starts_with("start") {
                 Command::new("cmd").arg("/c").arg(line.replacen("start", "", 1)).spawn().expect("");
             } else {
-                let output = Command::new("cmd").arg("/c").arg(line.replace("<backslash>", "\\").replace("<slash>", "/")).output().expect("");
+                let output = Command::new("cmd").arg("/c").arg(line).output().expect("");
                 io::stdout().write_all(&output.stdout).unwrap();
                 io::stdout().write_all(&output.stderr).unwrap();
             }
@@ -209,7 +188,7 @@ fn main() {
             if line.ends_with("&") {
                 Command::new("sh").arg("-c").arg(line.replacen("&", "", 1)).spawn().expect("");
             } else {
-                let output = Command::new("sh").arg("-c").arg(line.replace("<backslash>", "\\").replace("<slash>", "/")).output().expect("");
+                let output = Command::new("sh").arg("-c").arg(line).output().expect("");
                 io::stdout().write_all(&output.stdout).unwrap();
                 io::stdout().write_all(&output.stderr).unwrap();
             }
